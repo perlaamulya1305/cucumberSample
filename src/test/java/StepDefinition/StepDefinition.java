7ff26843acb6d48ea8c76e388b9ce3ab36b32547
Amulya <amuqa3@gmail.com>
2018-01-05 11:01:54 -0500

    initial commit

---
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -0,0 +1,32 @@
+package StepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+    @Given("^I want to Login to salesforce$")
+    public void i_want_to_Login_to_salesforce() throws Throwable {
+
+    }
+
+    @Given("^enter into homepage$")
+    public void enter_into_homepage() throws Throwable {
+
+    }
+
+    @When("^username and password is entered$")
+    public void username_and_password_is_entered() throws Throwable {
+
+    }
+
+    @Then("^I validate the outcomes$")
+    public void i_validate_the_outcomes() throws Throwable {
+
+    }
+
+    @Then("^Logout the application$")
+    public void logout_the_application() throws Throwable {
+
+    }
+}
---
+++ src/test/java/TestRunner/TestRunner.java
@@ -0,0 +1,14 @@
+package TestRunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Feature"
+        ,glue={"StepDefinition"}
+)
+public class TestRunner {
+
+}
